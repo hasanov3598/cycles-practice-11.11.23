3c92212a6ab36a24e0fb1614e11e62c93d6b9a3d
hasanov3598 <bahramov3598@gmail.com>
2023-11-11 21:39:59 +0600

    commit

---
+++ bin/cycles.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main() {
+// //1
+//   for (int i = 0; i <= 20; i++) {
+//     if (i % 2 == 1) {
+//       continue;
+//     }
+//     print(i);
+//   }
+
+//2
+  // for (int i = 1; i < 9; i++) {
+  //   print("$i * $i = ${i * i} ");
+  // }
+
+  //3
+  // print('Enter N');
+  // int N = int.parse(stdin.readLineSync()!);
+
+  // int result = 1;
+  // for (int i = 1; i <= 5; i++) {
+  //   result *= i;
+  // }
+
+  // print(' $N');
+  // print(result);
+  //4
+  // List<int> nums = [10, 5, 8, 3, 12];
+
+  // int min = nums[0];
+  // int max = nums[0];
+
+  // for (int num in nums) {
+  //   if (num < min) {
+  //     min = num;
+  //   }
+  //   if (num > max) {
+  //     max = num;
+  //   }
+  // }
+
+  // print('Наименьший элемент: $min');
+  // print('Наибольший элемент: $max');
+  //5
+  // List<int> values = [10, 20, 30, 40, 50];
+  // double number1 = 0;
+
+  // for (int value in values) {
+  //   number1 += value;
+  // }
+
+  // double number = number1 / values.length;
+
+  //print('Среднее значение: $number');
+  //6
+  // List<int> numbers = [2, 3, 4, 2, 5, 2, 6];
+  // int num1 = 0;
+
+  // for (int number in numbers) {
+  //   if (number == 2) {
+  //     num1++;
+  //   }
+
+  // }
+
+  // print("$num1 ");
+}
